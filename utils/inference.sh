1da30d93d3893478b01efb01ff38a3d642e0d1c6
Ricky <Ricky@Hosfelt.io>
2019-11-21 15:23:46 -0500

    Updated inference script to not catch the SIGCHDKILL signal which was impacting some users

--- utils/inference.sh
+++ utils/inference.sh
@@ -21,7 +21,7 @@ set -euo pipefail
 function trap_ctrlc ()
 {
     # perform cleanup here
-    echo "Ctrl-C or Error caught...performing clean up"
+    echo "Ctrl-C or Error caught...performing clean up check /tmp/inference.log"
 
     if [ -d /tmp/inference ]; then
            rm -rf /tmp/inference
@@ -32,7 +32,7 @@ function trap_ctrlc ()
 
 # initialise trap to call trap_ctrlc function
 # when signal 2 (SIGINT) is received
-trap "trap_ctrlc" 2 9 13 3 17
+trap "trap_ctrlc" 2 9 13 3
 
 help_message () {
         printf "${0}: Runs the polygonization in inference mode\n\t-x: path to xview-2 repository\n\t-i: /full/path/to/input/pre-disaster/image.png\n\t-p: /full/path/to/input/post-disaster/image.png\n\t-o: /path/to/output.png\n\t-l: path/to/localization_weights\n\t-c: path/to/classification_weights\n\t-e /path/to/virtual/env/activate\n\t-y continue with local environment and without interactive prompt\n\n"
@@ -132,7 +132,7 @@ cd "$XBDIR"/spacenet/inference/
 
 # Quietly running the localization inference to output a json with the predicted polygons from the supplied input image
 printf "Running localization\n"
-python ./inference.py --input "$input" --weights "$localization_weights" --mean "$XBDIR"/weights/mean.npy --output "$label_temp"/"${input_image%.*}".json >> "$LOGFILE" 2>&1
+python3.7 ./inference.py --input "$input" --weights "$localization_weights" --mean "$XBDIR"/weights/mean.npy --output "$label_temp"/"${input_image%.*}".json >> "$LOGFILE" 2>&1
 
 printf "\n" >> "$LOGFILE"
 
@@ -150,25 +150,26 @@ mkdir -p "$inference_base"/output_polygons
 printf "Running classification\n" 
 
 # Extracting polygons from post image 
-python ./process_data_inference.py --input_img "$disaster_post_file" --label_path "$label_temp"/"${input_image%.*}".json --output_dir "$inference_base"/output_polygons --output_csv "$inference_base"/output.csv >> "$LOGFILE" 2>&1
+python3.7 ./process_data_inference.py --input_img "$disaster_post_file" --label_path "$label_temp"/"${input_image%.*}".json --output_dir "$inference_base"/output_polygons --output_csv "$inference_base"/output.csv >> "$LOGFILE" 2>&1
 
 # Classifying extracted polygons 
-python ./damage_inference.py --test_data "$inference_base"/output_polygons --test_csv "$inference_base"/output.csv --model_weights "$classification_weights" --output_json /tmp/inference/classification_inference.json >> "$LOGFILE" 2>&1
+python3.7 ./damage_inference.py --test_data "$inference_base"/output_polygons --test_csv "$inference_base"/output.csv --model_weights "$classification_weights" --output_json /tmp/inference/classification_inference.json >> "$LOGFILE" 2>&1
 
 printf "\n" >> "$LOGFILE"
 
 # Combining the predicted polygons with the predicted labels, based off a UUID generated during the localization inference stage  
 printf "Formatting json and scoring image\n"
-python "$XBDIR"/utils/combine_jsons.py --polys "$label_temp"/"${input_image%.*}".json --classes /tmp/inference/classification_inference.json --output "$inference_base/inference.json" >> "$LOGFILE" 2>&1
+python3.7 "$XBDIR"/utils/combine_jsons.py --polys "$label_temp"/"${input_image%.*}".json --classes /tmp/inference/classification_inference.json --output "$inference_base/inference.json" >> "$LOGFILE" 2>&1
 printf "\n" >> "$LOGFILE"
 
 # Transforming the inference json file to the image required for scoring
-printf "Finalizing output to $output_file\n" 
-python "$XBDIR"/utils/inference_image_output.py --input "$inference_base"/inference.json --output "$output_file"  >> "$LOGFILE" 2>&1
+printf "Finalizing output file" 
+python3.7 "$XBDIR"/utils/inference_image_output.py --input "$inference_base"/inference.json --output "$output_file"  >> "$LOGFILE" 2>&1
 
 #Cleaning up by removing the temporary working directory we created
 printf "Cleaning up\n"
 rm -rf "$inference_base"
+
 printf "==========\n" >> "$LOGFILE"
 printf "Done!\n"
 
